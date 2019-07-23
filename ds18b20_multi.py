c52631bc5b91f8bf11114a48cede6acf751a16ff
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-07-23 09:04:26 -0400

    Update ds18b20_multi.py

--- ds18b20_multi.py
+++ ds18b20_multi.py
@@ -1,39 +1,62 @@
+ 
 import os
 import glob
 import time
+import datetime
 
 #these tow lines mount the device:
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
-base_dir = '/sys/bus/w1/devices/'
-device_path = glob.glob(base_dir + '28*') #get list of file paths of sensors
+base_dir = r'/sys/bus/w1/devices/28*'
+device_path = glob.glob(base_dir) #get list of file paths of sensors
 rom = [d.split('/')[-1] for d in device_path] #get list of roms
 
-def read_temp_raw(i):
-    with open(device_path[i] +'/w1_slave','r') as f:
-        valid, temp = f.readlines()
-    return valid, temp
- 
-def read_temp():
-    
-    for i in range(len(rom)):
-        
-        valid, temp = read_temp_raw(i)
-        print('ROM: ' + rom[i])
-        while 'YES' not in valid:
-            time.sleep(0.2)
-            valid, temp = read_temp_raw()
+class DS18B20:
+    def __init__(self):
+        self.base_dir = r'C:\Users\us43060\Desktop\GitHub\Raspberry-Pi-Sensors\sys\bus\w1\devices\28*'
+        self.sensor_path = []        
+        self.sensor_name = []
+        self.temps = []
+        self.log = []
 
-        pos = temp.index('t=')
-        if pos != -1:
-            #read the temperature .
-            temp_string = temp[pos+2:]
-            temp_c = float(temp_string)/1000.0 
-            temp_f = temp_c * (9.0 / 5.0) + 32.0
-            return temp_c, temp_f
- 
+    def find_sensors(self):
+        self.sensor_path = glob.glob(self.base_dir)
+        self.sensor_name = [path.split('\\')[-1] for path in self.sensor_path]
+
+    def strip_string(self, temp_str):
+        i = temp_str.index('t=')
+        if i != -1:
+            t = temp_str[i+2:]
+            temp_c = float(t)/1000.0
+            temp_f = temp_c * (9.0/5.0) + 32.0
+        return temp_c, temp_f
+
+    def read_temp(self):
+        tstamp = datetime.datetime.now()
+        for sensor, path in zip(self.sensor_name, self.sensor_path):
+            # open sensor file and read data
+            with open(path + '/w1_slave','r') as f:
+                valid, temp = f.readlines()
+            # check validity of data
+            if 'YES' in valid:
+                self.log.append((tstamp, sensor) + self.strip_string(temp))
+                time.sleep(2)
+            else:
+                time.sleep(0.2)
+    
+    def print_temps(self):
+        print('-'*90)
+        for t, n, c, f in self.log:
+            print(f'Sensor: {n}    C={c:,.3f}    F={f:,.3f}    DateTime: {t}')
+
+    def clear_log(self):
+        s.log.clear()
+
+s = DS18B20()
+s.find_sensors()
+
 while True:
-    c, f = read_temp()
-    print(' C={:,.3f} F={:,.3f}\n'.format(c, f))
-    time.sleep(1)
+    s.read_temp()
+    s.print_temps()
+    s.clear_log()
